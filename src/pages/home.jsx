efd652ff563ab90ba33f12ce0a6dd6a04c069111
Ramaaeln <abdullahramadanelansary@gmail.com>
2025-09-26 10:38:10 +0700

    update:responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,44 @@
-import Background from '../assets/home/BG-SCHOOL.jpg'
+import Background from "../assets/home/BG-SCHOOL.jpg";
 
 export default function HomePage() {
-  return( 
-    <div className="
-    2xl:h-[624px] 
-    md:h-[624px] 
-    absolute w-full top-10 h-[624px] overflow-hidden"> 
-      <img 
-        src={Background} 
-        alt="Latar Belakang Sekolah Al-Amanah" 
-        className="w-full h-[624px] object-cover object-[20%_30%] rounded-b-xl" 
+  return (
+    <div
+      className="
+    2xl:h-[624px]
+    xl:h-[600px] 
+    md:h-[400px] 
+    absolute w-full top-10  overflow-hidden"
+    >
+      <img
+        src={Background}
+        alt="Latar Belakang Sekolah Al-Amanah"
+        className="w-full  
+        2xl:h-[924px]
+        xl:h-[500px] 
+        md:h-[300px]
+        object-cover 
+        object-[20%_30%] rounded-b-xl"
       />
-      
+
       <div className="absolute inset-0 flex flex-col justify-center z-10 items-center text-white text-center">
-         <h1 className="text-5xl sm:text-yellow-500 md:text-red-500 xl:text-white 2xl:text-black font-bold">SELAMAT DATANG</h1>
-         <h1 className="text-5xl font-extrabold mt-2">DI SMP AL-AMANAH</h1>
+        <h1 className="text-5xl 
+        sm:text-yellow-500 
+        md:text-red-500 
+        xl:text-blue-900 
+        2xl:text-black 
+        
+        font-bold">
+          SELAMAT DATANG
+        </h1>
+        <h1 className="text-5xl font-extrabold mt-2">DI SMP AL-AMANAH</h1>
       </div>
-      <div className='absolute inset-0 flex layer-home opacity-40 rounded-b-xl'></div>
+      <div
+        className="   
+                2xl:h-[624px]
+                xl:h-[500px]  
+                md:h-[300px] 
+                absolute inset-0 flex layer-home opacity-40 rounded-b-xl"
+      ></div>
     </div>
-  )
+  );
 }
